c6076a376d726cca797940b160f70b07935a10ef
Mark deVilliers <markdevilliers@gmail.com>
2013-09-10 20:44:37 +0100

    Solution for Étude 5-2: Using the re Module

--- etudes/5/dates.erl
+++ etudes/5/dates.erl
@@ -1,8 +1,13 @@
 -module (dates).
 -export ([date_parts/1]).
 
-
+-spec(date_parts(string()) -> {integer(),integer(),integer()}).
 date_parts(DateStr) ->
-	re:split(DateStr, "[-]", [{return,list}]).
-
-
+	case re:split(DateStr, "[-]", [{return,list}]) of
+		[Year,Month,Day]  ->
+			{YearInt, _} = string:to_integer(Year),
+			{MonthInt, _} = string:to_integer(Month),
+			{DayInt, _} = string:to_integer(Day),
+			{YearInt,MonthInt,DayInt};
+		_ -> error
+	end.
