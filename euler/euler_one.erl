f6a221775fc85c9b2525bc7fd8cea570ac7610f1
Mark deVilliers <markdevilliers@gmail.com>
2013-10-29 09:47:02 +0000

    Adding in some old euler examples

---
+++ euler/euler_one.erl
@@ -0,0 +1,22 @@
+-module(euler_one).
+-export([euler/1,euler/0]).
+
+% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+% Find the sum of all the multiples of 3 or 5 below 1000.
+
+euler() ->
+	euler(999).
+euler(N) ->
+	solve(N - 1,0).
+
+solve(0,Acc) ->
+	Acc;
+solve(N,Acc) ->
+	if
+		N rem 3 == 0 ->
+			solve(N-1, Acc + N);
+		N rem 5 == 0 ->
+			solve(N-1, Acc + N);
+		true ->
+			solve(N-1, Acc)
+	end.
