8a0d1c5f6df1cad42c485d14cede1bfdc4ea565d
Mark deVilliers <markdevilliers@gmail.com>
2013-12-17 14:39:24 +0000

    Played with the setup macro to instantiate an application instance

--- reliable-delivery/test/ack_tests.erl
+++ reliable-delivery/test/ack_tests.erl
@@ -3,10 +3,24 @@
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
  
-monitor_then_wait_then_ack_test() ->
+-define(setup(F), {setup, fun start/0, fun stop/1, F}).
+-define(foreach(Tests), {foreach, fun start/0, Tests}).
+
+start() ->
+	reliable_delivery:start().
+stop(_) ->
+	application:stop(reliable_delivery).
+
+running_application_test_() ->
+	[{"Tests requiring running reliable_delivery application",
+		?setup(
+			[
+			fun monitor_then_wait_then_ack_and_ack_again/0,
+			fun monitor_ack_unknown_monitor/0
+			])}].
+
+monitor_then_wait_then_ack_and_ack_again() ->
     
-	reliable_delivery:start(),
-	
 	{ok, Identifier} =  reliable_delivery:monitor(5000,<<"my application name">>, <<"my value">>),
 
 	timer:sleep(100),
@@ -14,8 +28,11 @@ monitor_then_wait_then_ack_test() ->
 	{ok,{ identifier, Identifier}} = reliable_delivery:ack(Identifier),
 
 	% attempt double ack
-	{already_acked,{ identifier, Identifier} } = reliable_delivery:ack(Identifier),
+	{already_acked,{ identifier, Identifier} } = reliable_delivery:ack(Identifier).
 
-	application:stop(reliable_delivery).
+monitor_ack_unknown_monitor() ->
+    
+	Identifier = <<"rubbish">>,
+	{error, {identifier_not_found, Identifier }} = reliable_delivery:ack(Identifier).
  
 -endif.
